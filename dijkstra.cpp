ace8e80a681d351e7dcda6e83b8867f4ff4d7baa
Michał <michauz2001@gmail.com>
2023-06-02 00:05:23 +0200

    3

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -5,90 +5,235 @@
 #include <fstream>
 #include <vector>
 #include <json-c/json.h>
+#include "dijkstra.h"
+#include <omp.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
-#define MAX_VERTICES 100
+using namespace std;
 
-struct Edge {
-    int source;
-    int destination;
-    int weight;
-};
+void dijkstraSerial(Graph* graph, int source) {
+    int vertices = graph->vertices;
 
-struct Graph {
-    int vertices;
-    int edges;
-    int adjacencyMatrix[MAX_VERTICES][MAX_VERTICES];
-};
+    // Inicjalizacja odległości jako nieskończoność dla wszystkich wierzchołków
+    std::vector<int> distances(vertices, INT_MAX);
 
-void convertToEdgeList(Graph* graph, std::vector<Edge>& edgeList) {
-    int index = 0;
+    // Ustawienie odległości źródłowego wierzchołka na 0
+    distances[source] = 0;
 
-    for (int i = 0; i < graph->vertices; i++) {
-        for (int j = i + 1; j < graph->vertices; j++) {
-            if (graph->adjacencyMatrix[i][j] != 0) {
-                Edge edge;
-                edge.source = i;
-                edge.destination = j;
-                edge.weight = graph->adjacencyMatrix[i][j];
-                edgeList.push_back(edge);
-                index++;
+    // Tablica oznaczająca, czy dany wierzchołek został już odwiedzony
+    std::vector<bool> visited(vertices, false);
+
+    // Główna pętla algorytmu Dijkstry
+    for (int i = 0; i < vertices - 1; ++i) {
+        // Znalezienie wierzchołka o najmniejszej odległości z nieodwiedzonych wierzchołków
+        int minDistance = INT_MAX;
+        int minIndex = -1;
+        for (int j = 0; j < vertices; ++j) {
+            if (!visited[j] && distances[j] < minDistance) {
+                minDistance = distances[j];
+                minIndex = j;
+            }
+        }
+
+        // Oznaczenie znalezionego wierzchołka jako odwiedzony
+        visited[minIndex] = true;
+
+        // Aktualizacja odległości dla sąsiadów znalezionego wierzchołka
+        for (const Edge& edge : graph->edges) {
+            if (edge.source == minIndex && !visited[edge.destination] &&
+                distances[minIndex] != INT_MAX &&
+                distances[minIndex] + edge.weight < distances[edge.destination]) {
+                distances[edge.destination] = distances[minIndex] + edge.weight;
+            }
+        }
+    }
+}
+
+
+void dijkstraOpenMP(Graph* graph, int source) {
+    int vertices = graph->vertices;
+
+    // Inicjalizacja odległości jako nieskończoność dla wszystkich wierzchołków
+    std::vector<int> distances(vertices, INT_MAX);
+
+    // Ustawienie odległości źródłowego wierzchołka na 0
+    distances[source] = 0;
+
+    // Tablica oznaczająca, czy dany wierzchołek został już odwiedzony
+    std::vector<bool> visited(vertices, false);
+
+    // Główna pętla algorytmu Dijkstry
+    for (int i = 0; i < vertices - 1; ++i) {
+        // Znalezienie wierzchołka o najmniejszej odległości z nieodwiedzonych wierzchołków
+        int minDistance = INT_MAX;
+        int minIndex = -1;
+
+        // Rozpoczęcie równoległej sekcji
+        #pragma omp parallel
+        {
+            int localMinDistance = INT_MAX;
+            int localMinIndex = -1;
+
+            // Rozpoczęcie równoległej iteracji po wierzchołkach
+            #pragma omp for
+            for (int j = 0; j < vertices; ++j) {
+                if (!visited[j] && distances[j] < localMinDistance) {
+                    localMinDistance = distances[j];
+                    localMinIndex = j;
+                }
+            }
+
+            // Znalezienie lokalnego minimum dla bieżącego wątku
+            #pragma omp critical
+            {
+                if (localMinDistance < minDistance) {
+                    minDistance = localMinDistance;
+                    minIndex = localMinIndex;
+                }
+            }
+        }
+
+        // Oznaczenie znalezionego wierzchołka jako odwiedzony
+        visited[minIndex] = true;
+
+        // Aktualizacja odległości dla sąsiadów znalezionego wierzchołka
+        #pragma omp parallel for
+        for (int j = 0; j < graph->edges.size(); ++j) {
+            const Edge& edge = graph->edges[j];
+            if (edge.source == minIndex && !visited[edge.destination] &&
+                distances[minIndex] != INT_MAX &&
+                distances[minIndex] + edge.weight < distances[edge.destination]) {
+                distances[edge.destination] = distances[minIndex] + edge.weight;
             }
         }
     }
 
-    graph->edges = index;
+    // Wyświetlenie odległości dla wszystkich wierzchołków
+    for (int i = 0; i < vertices; ++i) {
+        std::cout << "Distance from vertex " << source << " to vertex " << i << ": " << distances[i] << std::endl;
+    }
 }
 
-void dijkstraSerial(Graph* graph, int source) {
-    cout << "serial";
-}
+__global__ void dijkstraCUDA(Graph* graph, int source, int* distances) {
+    int vertices = graph->vertices;
+    int* adjacencyMatrix = &(graph->adjacencyMatrix[0][0]);
 
-void dijkstraOpenMP(Graph* graph, int source) {
-    cout << "openmp";
-}
-
-void dijkstraCUDA(Graph* graph, int source) {
-    cout << "serial";
-}
-
-int main() {
-    const std::string filename = "graph.json";
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cout << "Błąd podczas otwierania pliku." << std::endl;
-        return 1;
+    int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+    if (threadId < vertices) {
+        distances[threadId] = INF;
     }
 
-    // Odczytanie pliku JSON
-    std::string buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    if (threadId == source) {
+        distances[threadId] = 0;
+    }
 
-    // Parsowanie pliku JSON
-    json_object* jsonObj = json_tokener_parse(buffer.c_str());
-    json_object* jsonVertices;
-    json_object* jsonAdjacencyMatrix;
+    __syncthreads();
 
-    Graph graph;
-    std::vector<Edge> edgeList;
+    for (int i = 0; i < vertices - 1; ++i) {
+        int minDist = INF;
+        int minIndex = -1;
 
-    // Odczytanie liczby wierzchołków
-    json_object_object_get_ex(jsonObj, "vertices", &jsonVertices);
-    graph.vertices = json_object_get_int(jsonVertices);
+        for (int j = 0; j < vertices; ++j) {
+            if (!visited[j] && distances[j] < minDist) {
+                minDist = distances[j];
+                minIndex = j;
+            }
+        }
 
-    // Odczytanie macierzy sąsiedztwa
-    json_object_object_get_ex(jsonObj, "adjacencyMatrix", &jsonAdjacencyMatrix);
-    int i, j;
-    json_object* jsonRow;
-    json_object* jsonWeight;
-    for (i = 0; i < graph.vertices; i++) {
-        jsonRow = json_object_array_get_idx(jsonAdjacencyMatrix, i);
-        for (j = 0; j < graph.vertices; j++) {
-            jsonWeight = json_object_array_get_idx(jsonRow, j);
-            graph.adjacencyMatrix[i][j] = json_object_get_int(jsonWeight);
+        visited[minIndex] = true;
+
+        for (int k = 0; k < vertices; ++k) {
+            int edgeWeight = adjacencyMatrix[minIndex * vertices + k];
+            if (!visited[k] && edgeWeight && (distances[minIndex] + edgeWeight < distances[k])) {
+                distances[k] = distances[minIndex] + edgeWeight;
+            }
         }
     }
+}
 
-    // Konwersja macierzy sąsiedztwa na listę krawędzi
-    convertToEdgeList(&graph, edgeList);
+void dijkstraCUDAWrapper(Graph* graph, int source) {
+    int vertices = graph->vertices;
+    int* distances = new int[vertices];
+    int* cudaDistances;
+
+    cudaMalloc((void**)&cudaDistances, vertices * sizeof(int));
+
+    cudaMemcpy(cudaDistances, distances, vertices * sizeof(int), cudaMemcpyHostToDevice);
+
+    dijkstraCUDA<<<(vertices + 255) / 256, 256>>>(graph, source, cudaDistances);
+
+    cudaMemcpy(distances, cudaDistances, vertices * sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaFree(cudaDistances);
+
+    // Wypisz wyniki
+    for (int i = 0; i < vertices; ++i) {
+        std::cout << "Vertex: " << i << ", Distance: " << distances[i] << std::endl;
+    }
+
+    delete[] distances;
+}
+
+Graph loadGraphFromFile(const char* filename) {
+    Graph graph;
+    graph.vertices = 0;
+    graph.edges.clear();
+
+    FILE* file = fopen(filename, "r");
+    if (file != nullptr) {
+        fseek(file, 0, SEEK_END);
+        long fileSize = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        char* buffer = (char*)malloc(fileSize + 1);
+        if (buffer != nullptr) {
+            fread(buffer, 1, fileSize, file);
+            buffer[fileSize] = '\0';
+
+            json_object* jsonObj = json_tokener_parse(buffer);
+            if (jsonObj != nullptr) {
+                json_object* jsonVertices = nullptr;
+                if (json_object_object_get_ex(jsonObj, "vertices", &jsonVertices)) {
+                    graph.vertices = json_object_get_int(jsonVertices);
+                }
+
+                json_object* jsonEdges = nullptr;
+                if (json_object_object_get_ex(jsonObj, "edges", &jsonEdges)) {
+                    int numEdges = json_object_array_length(jsonEdges);
+                    for (int i = 0; i < numEdges; i++) {
+                        json_object* jsonEdge = json_object_array_get_idx(jsonEdges, i);
+                        json_object* jsonSource = nullptr;
+                        json_object* jsonDestination = nullptr;
+                        json_object* jsonWeight = nullptr;
+
+                        if (json_object_object_get_ex(jsonEdge, "source", &jsonSource) &&
+                            json_object_object_get_ex(jsonEdge, "destination", &jsonDestination) &&
+                            json_object_object_get_ex(jsonEdge, "weight", &jsonWeight)) {
+
+                            int source = json_object_get_int(jsonSource);
+                            int destination = json_object_get_int(jsonDestination);
+                            int weight = json_object_get_int(jsonWeight);
+
+                            graph.edges.push_back({source, destination, weight});
+                        }
+                    }
+                }
+            }
+
+            free(buffer);
+            json_object_put(jsonObj);
+        }
+
+        fclose(file);
+    }
+
+    return graph;
+}
+
+
+int dijkstra() {
+    Graph graph = loadGraphFromFile("graph_40.json");
 
     // Wywołanie algorytmu Dijkstry w różnych wersjach
     int sourceVertex = 0;
@@ -104,4 +249,3 @@ int main() {
 
     return 0;
 }
-
