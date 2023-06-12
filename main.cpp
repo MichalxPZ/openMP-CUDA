bb8dd662f593aa65611e54b9264428682e75c04a
Michał <michauz2001@gmail.com>
2023-06-12 19:09:06 +0200

    generowanie losowych grafow #2

--- main.cpp
+++ main.cpp
@@ -1,14 +1,54 @@
-//
-// Created by Michał Zieliński on 01/06/2023.
-//
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <json-c/json.h>
 
+void generateRandomGraph(int vertices, int density, json_object* jsonArray) {
+    int maxEdges = (vertices * (vertices - 1)) / 2;
+    int numEdges = (density * maxEdges) / 100;
 
-#include <iostream>
-#include "dijkstra.h"
-#include "graphGenerator.h"
+    // Generate all possible edges
+    int index = 0;
+    for (int i = 0; i < vertices - 1; i++) {
+        for (int j = i + 1; j < vertices; j++) {
+            struct json_object* jsonEdge = json_object_new_object();
+            json_object_object_add(jsonEdge, "source", json_object_new_int(i));
+            json_object_object_add(jsonEdge, "destination", json_object_new_int(j));
+            json_object_object_add(jsonEdge, "weight", json_object_new_int(rand() % 100 + 1));
+            json_object_array_add(jsonArray, jsonEdge);
+            index++;
+        }
+    }
+
+    // Remove random edges to achieve the desired density
+    int numEdgesToRemove = maxEdges - numEdges;
+    while (numEdgesToRemove > 0) {
+        int randomIndex = rand() % index;
+        json_object_array_del_idx(jsonArray, randomIndex, 1);
+        numEdgesToRemove--;
+        index--;
+    }
+}
+
 
 int main() {
-    generate();
-    dijkstra();
-    return  0;
+    srand(time(nullptr));
+
+    for (int vertices = 25; vertices <= 500; vertices += 25) {
+        json_object* jsonArray = json_object_new_array();
+        generateRandomGraph(vertices, 50, jsonArray);
+        char filename[100];
+        snprintf(filename, sizeof(filename), "graph_%d.json", vertices);
+        FILE* file = fopen(filename, "w");
+        if (file != nullptr) {
+            fprintf(file, "%s", json_object_to_json_string_ext(jsonArray, JSON_C_TO_STRING_PRETTY));
+            fclose(file);
+            printf("Saved graph to %s\n\n", filename);
+        } else {
+            printf("Failed to open file %s for writing.\n\n", filename);
+        }
+        json_object_put(jsonArray);
+    }
+
+    return 0;
 }
--- visualize.py
+++ visualize.py
@@ -1,26 +1,25 @@
-    import json
-    import networkx as nx
-    import matplotlib.pyplot as plt
+import json
+import networkx as nx
+import matplotlib.pyplot as plt
 
-    def visualizeGraph(filename):
-        with open(filename, 'r') as file:
-            data = json.load(file)
+def visualizeGraph(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
 
-        vertices = data['vertices']
-        adjacencyMatrix = data['adjacencyMatrix']
+    edges = data
 
-        G = nx.Graph()
-        G.add_nodes_from(range(vertices))
+    G = nx.Graph()
 
-        for i in range(vertices):
-            for j in range(i + 1, vertices):
-                if adjacencyMatrix[i][j] == 1:
-                    G.add_edge(i, j)
+    for edge in edges:
+        source = edge['source']
+        destination = edge['destination']
+        weight = edge['weight']
+        G.add_edge(source, destination, weight=weight)
 
-        pos = nx.spring_layout(G)
-        nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, font_size=10, font_weight='bold', edge_color='gray')
-        plt.title('Graph Visualization')
-        plt.show()
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, font_size=10, font_weight='bold', edge_color='gray')
+    plt.title('Graph Visualization')
+    plt.show()
 
-    filename = 'cmake-build-debug/graph_30.json'
-    visualizeGraph(filename)
+filename = 'cmake-build-debug/graph.json'
+visualizeGraph(filename)
