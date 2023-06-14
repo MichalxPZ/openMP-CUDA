029ff2e7dc53f08a8f6329f2c5b51acd26f01d77
MichalxPZ <michauz2001@gmail.com>
2023-06-14 17:10:43 +0200

    CUDA RESULTS AND GRAPHS

--- results.py
+++ results.py
@@ -1,53 +1,92 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = {
-    'Size': [25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500],
-    'CUDA': [102604, 3246, 10168, 23830, 45289, 112962, 166959, 158138, 223841, 307171, 407747, 530189, 672121, 839438, 1032150, 1252047, 1501122, 1781083, 2093780, 2440467],
-    'Serial': [1006, 1010, 3001, 4009, 6001, 8994, 11989, 16002, 19996, 25991, 32996, 39999, 49000, 56998, 68001, 78987, 114004, 1006, 1006, 1006],
-    'OPENMP': [27000, 24999, 27990, 27993, 26991, 21003, 25001, 28006, 32991, 36999, 64998, 65000, 73982, 80000, 100977, 109002, 90014, 100977, 109002, 90014]
+    'Vertices': [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    'Serial': [68212, 1052945, 5241891, 18512200, 42976821, 90724379, 167186474, 285444734, 456163822],
+    'CUDA': [216134, 294669, 691272, 1401878, 2597696, 4381982, 6863220, 10118992, 14345267]
 }
 
 df = pd.DataFrame(data)
-df['Serial'] = df['Serial'] * df['Size']
-df['OPENMP'] = df['OPENMP'] * df['Size']
+df['Serial'] /= 1000  # Convert to milliseconds
+df['CUDA'] /= 1000  # Convert to milliseconds
 
+plt.figure(figsize=(10, 6))
+plt.plot(df['Vertices'], df['Serial'], marker='o', label='Serial')
+plt.plot(df['Vertices'], df['CUDA'], marker='o', label='CUDA')
+
+plt.title('Execution Time Comparison: Serial vs CUDA')
+plt.xlabel('Vertices')
+plt.ylabel('Time (ms)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+serial_times = df['Serial']
+df['Speedup'] = serial_times / df['CUDA']
+
+# Print DataFrame
 print(df)
 
-import matplotlib.pyplot as plt
+# Plot speedup
+plt.figure(figsize=(10, 6))
+plt.plot(df['Vertices'], df['Speedup'], marker='o')
 
-# Utwórz wykres
-plt.plot(df['Size'], df['CUDA'], marker='o', label='CUDA')
-plt.plot(df['Size'], df['Serial'], marker='o', label='Serial')
-plt.plot(df['Size'], df['OPENMP'], marker='o', label='OPENMP')
-
-# Dodaj tytuł i etykiety osi
-plt.title('Execution Time Comparison')
-plt.xlabel('Size')
-plt.ylabel('Execution Time (microseconds)')
-
-# Dodaj legendę
-plt.legend()
-
-# Wyświetl wykres
+plt.title('Speedup Comparison: Serial vs CUDA')
+plt.xlabel('Vertices')
+plt.ylabel('Speedup')
+plt.grid(True)
 plt.show()
 
-df['OpenMP Acceleration'] = df['Serial'] / df['OPENMP']
 
-# Obliczenie przyspieszenia dla CUDA
-df['CUDA Acceleration'] = df['Serial'] / df['CUDA']
+results = [
+    (1000, "Serial", 848538),
+    (1000, "OPENMP", 335997),
+    (2000, "Serial", 6824267),
+    (2000, "OPENMP", 3626471),
+    (3000, "Serial", 24332369),
+    (3000, "OPENMP", 14290351),
+    (4000, "Serial", 55994073),
+    (4000, "OPENMP", 33425967),
+    (5000, "Serial", 110926232),
+    (5000, "OPENMP", 67367441),
+    (6000, "Serial", 183704272),
+    (6000, "OPENMP", 121806870),
+    (7000, "Serial", 328801811),
+    (7000, "OPENMP", 179628613),
+    (8000, "Serial", 443322364),
+    (8000, "OPENMP", 269045048),
+    (9000, "Serial", 625262705),
+    (9000, "OPENMP", 359967367)
+]
 
-# Wykres przyspieszenia dla OpenMP
-plt.plot(df['Size'], df['OpenMP Acceleration'], label='OpenMP')
-plt.xlabel('Size')
-plt.ylabel('Acceleration')
-plt.title('OpenMP Acceleration')
+df = pd.DataFrame(results, columns=['Vertices', 'Method', 'Time'])
+df['Time'] /= 1000  # Convert to milliseconds
+
+plt.figure(figsize=(10, 6))
+for method in df['Method'].unique():
+    df_method = df[df['Method'] == method]
+    plt.plot(df_method['Vertices'], df_method['Time'], marker='o', label=method)
+
+plt.title('Execution Time Comparison: Serial vs OPENMP')
+plt.xlabel('Vertices')
+plt.ylabel('Time (ms)')
 plt.legend()
+plt.grid(True)
 plt.show()
 
-# Wykres przyspieszenia dla CUDA
-plt.plot(df['Size'], df['CUDA Acceleration'], label='CUDA')
-plt.xlabel('Size')
-plt.ylabel('Acceleration')
-plt.title('CUDA Acceleration')
-plt.legend()
+serial_times = df[df['Method'] == 'Serial']['Time']
+df['Speedup'] = serial_times.values / df['Time'].values
+
+# Print DataFrame
+print(df)
+
+# Plot speedup
+plt.figure(figsize=(10, 6))
+plt.plot(df['Vertices'], df['Speedup'], marker='o')
+
+plt.title('Speedup Comparison: Serial vs OPENMP')
+plt.xlabel('Vertices')
+plt.ylabel('Speedup')
+plt.grid(True)
 plt.show()
